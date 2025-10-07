5cc6416a396029038b38912a07190a7e60cec90c
m8tan <alt6526@gmail.com>
2025-10-07 21:45:31 +0300

    e

--- PushToGit.bat
+++ PushToGit.bat
@@ -2,10 +2,7 @@
 cd C:\Projects\Wordpasser
 git add .
 set /p Message="Enter commit message:"
-echo %Message%
-pause
 git commit -m %Message%
 git push
 echo Done!
 timeout /t 3 /nobreak>nul
-cmd /k
